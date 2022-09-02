8216204240b372b18aa369e7c10e19207bff5eb7
Parham2002 <parhamm2002@gmail.com>
2022-09-02 10:36:13 +0100

    Set up jest and babel

---
+++ translator.spec.js
@@ -0,0 +1,13 @@
+import { translateToMorse } from "./translator";
+
+describe('Testing translateToMorse', () => {
+  it("2 should be 2", ()=> {
+    // Arrange
+      let number;
+    // Act
+      number = 5;
+    // Assert
+    expect(number).toBe(2)
+  })
+
+ })
