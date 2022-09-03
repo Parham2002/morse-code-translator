d2380553fb1158545fe967e53b07730c48269ed2
Parham2002 <parhamm2002@gmail.com>
2022-09-03 11:16:43 +0100

    Added English to Morse dictionary

--- translator.js
+++ translator.js
@@ -1,3 +1,151 @@
+const engMorseDictionary = [
+    {
+        english: "A",
+        morse: ".-"
+    },
+    {
+        english: "B",
+        morse: "-..."
+    },
+    {
+        english: "C",
+        morse: "-.-."
+    },
+    {
+        english: "D",
+        morse: "-.."
+    },
+    {
+        english: "E",
+        morse: "."
+    },
+    {
+        english: "F",
+        morse: "..-."
+    },
+    {
+        english: "G",
+        morse: "--."
+    },
+    {
+        english: "H",
+        morse: "...."
+    },
+    {
+        english: "I",
+        morse: ".."
+    },
+    {
+        english: "J",
+        morse: ".---"
+    },
+    {
+        english: "K",
+        morse: "-.-"
+    },
+    {
+        english: "L",
+        morse: ".-.."
+    },
+    {
+        english: "M",
+        morse: "--"
+    },
+    {
+        english: "N",
+        morse: "-."
+    },
+    {
+        english: "O",
+        morse: "---"
+    },
+    {
+        english: "P",
+        morse: "--"
+    },
+    {
+        english: "Q",
+        morse: "--.-"
+    },
+    {
+        english: "R",
+        morse: ".-."
+    },
+    {
+        english: "S",
+        morse: "..."
+    },
+    {
+        english: "T",
+        morse: "-"
+    },
+    {
+        english: "U",
+        morse: "..-"
+    },
+    {
+        english: "V",
+        morse: "...-"
+    },
+    {
+        english: "W",
+        morse: ".--"
+    },
+    {
+        english: "X",
+        morse: "-..-"
+    },
+    {
+        english: "Y",
+        morse: "-.--"
+    },
+    {
+        english: "Z",
+        morse: "--.."
+    },
+    {
+        english: "1",
+        morse: ".----"
+    },
+    {
+        english: "2",
+        morse: "..---"
+    },
+    {
+        english: "3",
+        morse: "...--"
+    },
+    {
+        english: "4",
+        morse: "....-"
+    },
+    {
+        english: "5",
+        morse: "....."
+    },
+    {
+        english: "6",
+        morse: "-...."
+    },
+    {
+        english: "7",
+        morse: "--..."
+    },
+    {
+        english: "8",
+        morse: "---.."
+    },
+    {
+        english: "9",
+        morse: "----."
+    },
+    {
+        english: "0",
+        morse: "-----"
+    },
+
+]
+const userInput = document.querySelector(".input")
 export const translateToMorse = () => {
     
 }
--- translator.spec.js
+++ translator.spec.js
@@ -1,13 +1,81 @@
 import { translateToMorse } from "./translator";
 
+// #1
 describe('Testing translateToMorse', () => {
-  it("2 should be 2", ()=> {
-    // Arrange
-      let number;
-    // Act
-      number = 5;
-    // Assert
-    expect(number).toBe(2)
+  it("'A' should be '.-'", ()=> {
+    expect().toBe()
   })
 
- })
+})
+
+// #2
+describe('Testing translateToMorse', () => {
+  it("'hello' should be '.... . .-.. .-.. ---'", ()=> {
+    expect().toBe()
+  })
+
+})
+
+// #3
+describe('Testing translateToMorse', () => {
+  it("2 should be 2", ()=> {
+    expect().toBe()
+  })
+
+})
+
+// #4
+describe('Testing translateToMorse', () => {
+  it("2 should be 2", ()=> {
+    expect().toBe()
+  })
+
+})
+
+// #5
+describe('Testing translateToMorse', () => {
+  it("2 should be 2", ()=> {
+    expect().toBe()
+  })
+
+})
+
+// #6
+describe('Testing translateToMorse', () => {
+  it("2 should be 2", ()=> {
+    expect().toBe()
+  })
+
+})
+
+// #7
+describe('Testing translateToMorse', () => {
+  it("2 should be 2", ()=> {
+    expect().toBe()
+  })
+
+})
+
+// #8
+describe('Testing translateToMorse', () => {
+  it("2 should be 2", ()=> {
+    expect().toBe()
+  })
+
+})
+
+// #9
+describe('Testing translateToMorse', () => {
+  it("2 should be 2", ()=> {
+    expect().toBe()
+  })
+
+})
+
+// #10
+describe('Testing translateToMorse', () => {
+  it("2 should be 2", ()=> {
+    expect().toBe()
+  })
+
+})
